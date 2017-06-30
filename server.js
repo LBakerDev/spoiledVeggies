f460f12ceeb864381fc5d85efd3b8e2d79676ebb
londonbaker4@gmail.com <londonbaker4@gmail.com>
2017-06-29 23:57:55 -0400

    added new post page and styled. Added show page, no styling

--- server.js
+++ server.js
@@ -9,38 +9,20 @@ mongoose.connect("mongodb://localhost/spoiledveggies");
 
 // App configuration
 app.set("view engine", "ejs");
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 
 // Mongoose model configuration
 
 const blogSchema = new mongoose.Schema({
-    title: {type: String, required: true},
-    image: {type: String, required: true},
-    body: {type: String, required: true},
+    title: {type: String, required: false},
+    image: {type: String, required: false},
+    body: {type: String, required: false},
     created: {type: Date, default: Date.now}
 });
 
 const Blog = mongoose.model("Blog", blogSchema);
-
-Blog.create({
-    title: "First Post",
-    image: "https://unsplash.com/?photo=JIISwPwxPD8",
-    body: "This is surely the first Post"
-})
-
-Blog.create({
-    title: "Second Post",
-    image: "https://unsplash.com/?photo=kQzFr1JqSKM",
-    body: "I promise this is the second Post"
-})
-
-Blog.create({
-    title: "Third Post",
-    image: "https://unsplash.com/?photo=kQzFr1JqSKM",
-    body: "There is no doubt that this is the third and final post"
-})
 
 
 //RESTful Routes
@@ -50,12 +32,58 @@ app.get("/", function(req, res) {
     res.redirect("/blogs");
 });
 
+// Index Route
 app.get("/blogs", function(req, res) {
     Blog.find({}, function(err, blogs) {
         if(err) {
             console.log("ERROR!");
         } else {
             res.render("index", {blogs: blogs});
+        }
+    })
+})
+
+// New Route
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+});
+
+// Create route
+app.post("/blogs", function(req,res) {
+    // create blog
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if(err){
+            console.log(err);
+            res.render("new");
+        } else {
+            // then, redirect to index
+            res.redirect("/blogs");
+            console.log("Post submitted")
+        }
+
+    })
+    
+})
+
+// Show route
+app.get("/blogs/:id", function(req,res) {
+   
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if(err) {
+            res.redirect('/blogs');
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    })
+})
+
+// Edit Route
+app.get("/blogs/:id/edit", function(req,res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
         }
     })
 })
