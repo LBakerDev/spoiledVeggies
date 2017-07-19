0deee92a75a782c2a50b9bcd7ded7bb7569f7a77
londonbaker4@gmail.com <londonbaker4@gmail.com>
2017-07-19 03:17:45 -0400

    updated url placeholder

--- config.js
+++ config.js
@@ -12,9 +12,9 @@ let config = {
         PORT: (process.env.PORT || 8080)
     },
     test: {
-        DATABASE_URL: 'mongodb://localhost/spoiledveggies-test', // Update to mlab test db
+        DATABASE_URL: 'mongodb://baker:baker@ds141082.mlab.com:41082/spoiledveggies-test', // Update to mlab test db
         PORT: (process.env.PORT || 8081)
-    }
+    },
 };
 
 module.exports = config[process.env.NODE_ENV || 'development'];
